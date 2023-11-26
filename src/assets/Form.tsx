7d0b850b63ebd3a9ab44e262215940a7f583c139
enzgamilla <laurenceangelogamilla@gmail.com>
2023-11-26 14:18:44 +0400

    log input data using react hook form

--- src/assets/Form.tsx
+++ src/assets/Form.tsx
@@ -1,35 +1,42 @@
-import { FormEvent, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const Form = () => {
   //   const nameRef = useRef<HTMLInputElement>(null);
   //   const ageRef = useRef<HTMLInputElement>(null);
   //   const person = { name: "", age: 0 };
 
-  const handleFormSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    // if (nameRef.current !== null) person.name = nameRef.current?.value;
-    // if (ageRef.current !== null) person.age = parseInt(ageRef.current?.value);
+  //   const handleFormSubmit = (event: FormEvent) => {
+  //     event.preventDefault();
+  // if (nameRef.current !== null) person.name = nameRef.current?.value;
+  // if (ageRef.current !== null) person.age = parseInt(ageRef.current?.value);
 
-    // console.log(person);
-    console.log(person);
+  // console.log(person);
+  //     console.log(person);
+  //   };
+
+  //   const [person, setPerson] = useState({
+  //     name: "",
+  //     age: "",
+  //   });
+
+  const { register, handleSubmit } = useForm();
+
+  const onSubmitData = (data: FieldValues) => {
+    console.log(data);
   };
 
-  const [person, setPerson] = useState({
-    name: "",
-    age: "",
-  });
-
   return (
-    <form onSubmit={handleFormSubmit}>
+    <form onSubmit={handleSubmit(onSubmitData)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          onChange={(event) => {
-            setPerson({ ...person, name: event.target.value });
-          }}
-          value={person.name}
+          //   onChange={(event) => {
+          //     setPerson({ ...person, name: event.target.value });
+          //   }}
+          //   value={person.name}
+          {...register("name")}
           id="name"
           type="text"
           className="form-control"
@@ -40,10 +47,11 @@ const Form = () => {
           Age
         </label>
         <input
-          onChange={(event) => {
-            setPerson({ ...person, age: event.target.value });
-          }}
-          value={person.age}
+          //   onChange={(event) => {
+          //     setPerson({ ...person, age: event.target.value });
+          //   }}
+          //   value={person.age}
+          {...register("age")}
           id="age"
           type="number"
           className="form-control"
